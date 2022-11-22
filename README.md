6f1ef69684215311b345299917e0ec92274db1ac
salih <muhammetsalihaslan@gmail.com>
2022-11-22 10:53:09 +0300

    first strucrute of all

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # stripe-app
-its a stripe payment method
+
+# what am i using
+Yarn
+React-router-dom
+Switch
+Stripe
+
+
+
+its a stripe payment method simple app
+
+shows us how to use stripe payment method
+i simple used yarn
+
+
+
+
--- stripeapp/src/App.js
+++ stripeapp/src/App.js
@@ -1,6 +1,19 @@
+import {  Routes, Route } from 'react-router-dom';
+import Pay from './pages/Pay';
+import Success from './pages/Success';
+
 
 function App() {
-  return 
+  return (
+  <Routes>
+    <Route>
+      <Pay path="/pay"/>
+    </Route>
+    <Route>
+      <Success path="/success"/>
+    </Route>
+  </Routes>
+  )
 }
 
 export default App;
