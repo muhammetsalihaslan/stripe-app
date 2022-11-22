d9a1305bf54f523979f880a3590bec9e720682ce
salih <muhammetsalihaslan@gmail.com>
2022-11-22 22:30:51 +0300

    success

--- stripeapp/src/pages/Success.jsx
+++ stripeapp/src/pages/Success.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Success = () => {
   return (
-    <div>Success</div>
+    <div style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }}>
+      <button style={{
+        border:"none",
+        width:120,
+        borderRadius:5,
+        padding:"20px",
+        backgroundColor:"teal",
+        color:"white",
+        fontWeight:"600",
+        cursor:"pointer",}}>
+        Success
+      </button>
+      <p>Your  payment succesfully get</p>
+    </div>
   )
 }
 
